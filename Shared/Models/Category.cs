7aa386c4e9d0f6a08d5d13902fa8f724f463408d
razvanr91 <razvanroman1991@gmail.com>
2021-12-26 12:22:36 +0000

    Create the Category model

---
+++ Shared/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Models
+{
+    internal class Category
+    {
+        [Key]
+        public Guid CategoryId { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public string ThumbnailImagePath { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Name { get; set; }
+
+        [Required] 
+        [MaxLength(1024)]
+        public string Description { get; set; }
+    }
+}
